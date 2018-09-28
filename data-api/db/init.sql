a66eedd04486ad0d6515958b9d7b5955b382b454
chao.chang <chao.chang@linecorp.com>
2018-09-28 18:21:28 +0900

    Add sql.

---
+++ data-api/db/init.sql
@@ -0,0 +1,43 @@
+--
+-- Sample dataset containing a number of cities)
+--
+DROP TABLE IF EXISTS city;
+
+create table city (
+id     bigint PRIMARY KEY,
+name    varchar(40),
+country    varchar(40)
+);
+
+insert into city(id, country, name) values (1, 'Australia', 'Brisbane');
+insert into city(id, country, name) values (2, 'Australia', 'Melbourne');
+insert into city(id, country, name) values (3, 'Australia', 'Sydney');
+insert into city(id, country, name) values (4, 'Canada', 'Montreal');
+
+insert into city(id, country, name) values (5, 'Israel', 'Tel Aviv');
+
+
+insert into city(id, country, name) values (6, 'Japan', 'Tokyo');
+
+
+insert into city(id, country, name) values (7, 'Spain', 'Barcelona');
+insert into city(id, country, name) values (8, 'Switzerland', 'Neuchatel');
+
+
+insert into city(id, country, name) values (9, 'UK', 'Bath');
+insert into city(id, country, name) values (10, 'UK', 'London');
+
+insert into city(id, country, name) values (11, 'UK', 'Southampton');
+insert into city(id, country, name) values (12, 'USA', 'Atlanta');
+insert into city(id, country, name) values (13, 'USA', 'Chicago');
+
+insert into city(id, country, name) values (14, 'USA', 'Eau Claire');
+insert into city(id, country, name) values (15, 'USA', 'Hollywood');
+insert into city(id, country, name) values (16, 'USA', 'Miami');
+insert into city(id, country, name) values (17, 'USA', 'Melbourne');
+insert into city(id, country, name) values (18, 'USA', 'New York');
+
+insert into city(id, country, name) values (19, 'USA', 'Palm Bay');
+insert into city(id, country, name) values (20, 'USA', 'San Francisco');
+
+insert into city(id, country, name) values (21, 'USA', 'Washington');
