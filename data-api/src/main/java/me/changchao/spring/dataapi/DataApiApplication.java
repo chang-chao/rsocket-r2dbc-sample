71c80b1fa021a5571e04ccfadd0529eb8f5fd3cf
chao.chang <chao.chang@linecorp.com>
2018-10-02 18:51:41 +0900

    Blocking server.

--- data-api/src/main/java/me/changchao/spring/dataapi/DataApiApplication.java
+++ data-api/src/main/java/me/changchao/spring/dataapi/DataApiApplication.java
@@ -1,6 +1,6 @@
 package me.changchao.spring.dataapi;
 
-import java.time.Instant;
+import java.time.Duration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -11,18 +11,17 @@ import org.springframework.context.ApplicationListener;
 import com.google.gson.Gson;
 
 import io.rsocket.AbstractRSocket;
+import io.rsocket.Closeable;
 import io.rsocket.Payload;
 import io.rsocket.RSocketFactory;
 import io.rsocket.SocketAcceptor;
 import io.rsocket.transport.netty.server.TcpServerTransport;
 import io.rsocket.util.DefaultPayload;
 import lombok.SneakyThrows;
-import lombok.extern.log4j.Log4j2;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @SpringBootApplication
-@Log4j2
 public class DataApiApplication implements ApplicationListener<ApplicationReadyEvent> {
 
 	@Autowired
@@ -31,7 +30,6 @@ public class DataApiApplication implements ApplicationListener<ApplicationReadyE
 	@SneakyThrows
 	public static void main(String[] args) {
 		SpringApplication.run(DataApiApplication.class, args);
-		System.in.read();
 	}
 
 	private Gson gson = new Gson();
@@ -46,8 +44,9 @@ public class DataApiApplication implements ApplicationListener<ApplicationReadyE
 			}
 		});
 
-		RSocketFactory.receive().acceptor(sa).transport(TcpServerTransport.create("localhost", 7000)).start()
-				.onTerminateDetach()
-				.subscribe(nettyContextCloseable -> log.info("started the server @ " + Instant.now().toString()));
+		Closeable server = RSocketFactory.receive().acceptor(sa).transport(TcpServerTransport.create("localhost", 7000))
+				.start().block();
+		server.onClose().block(Duration.ofDays(3650));
+
 	}
 }
